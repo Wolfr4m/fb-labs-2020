9010d35bdfec50e539b128a9d85c98092353498e
Anatolii <a.romanchenko@gmail.com>
2020-11-22 07:05:52 +0200

    searching key length

--- cp_2/Romanchenko_FB81/main.py
+++ cp_2/Romanchenko_FB81/main.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import re
 
 
@@ -14,13 +15,15 @@ def codable_things_of(alphabet):
     index_letter = {key: alphabet[key] for key in range(alphabet_length)}
     return letter_index, index_letter, alphabet_length
 
+
 def encode_with_keys(l_text, alphabet):
     keys = ["ад", "рай", "ключ", "жесть", "оченьбольшоеслово"]
-    print(l_text)
+    encoded_texts = []
     for key in keys:
-        print(f"Encoding text with {len(key)} length key {key}")
         encoded = encode(list(l_text), list(key), alphabet)
-        print(encoded)
+        encoded_texts.append(encoded)
+    return encoded_texts, keys
+
 
 def encode(l_text, l_key, alphabet):
     letter_index, index_letter, alphabet_length = codable_things_of(alphabet)
@@ -29,9 +32,44 @@ def encode(l_text, l_key, alphabet):
         l_text[index] = index_letter[(letter_index[l_text[index]] + letter_index[l_key[index % key_len]]) % alphabet_length]
     return "".join(l_text)
 
-alphabet = list(" абвгдежзийклмнопрстуфхцчшщъыьэюя")
 
-file = open("mytext.txt", "r", encoding="UTF-8")
+def decode(l_text, l_key, alphabet):
+    letter_index, index_letter, alphabet_length = codable_things_of(alphabet)
+    key_len = len(l_key)
+    for index in range(len(l_text)):
+        l_text[index] = index_letter[(letter_index[l_text[index]] - letter_index[l_key[index % key_len]]) % alphabet_length]
+    return "".join(l_text)
+
+
+def conformity_index(text):
+    letter_amount = len(text)
+    letter_and_amount = Counter(text)
+    letter_and_amount = list(sorted(letter_and_amount.items(), key=lambda t: t[0]))
+    print(letter_and_amount)
+    i_c = 0
+    for pair in letter_and_amount:
+        i_c += (pair[1] * (pair[1] - 1)) / (letter_amount * (letter_amount - 1))
+
+    return i_c
+
+
+def detect_possible_key_length(text):
+    pass
+
+
+russ_alphabet = list("абвгдежзийклмнопрстуфхцчшщъыьэюя")
+
+# file = open("mytext.txt", "r", encoding="UTF-8")
+# text = file.read().lower()
+#
+# print("\n" + str(text) + "\n")
+# encoded, keys = encode_with_keys(text, russ_alphabet)
+# for index in range(len(encoded)):
+#     i_c = conformity_index(encoded[index])
+#     print(f"Encoding text with {len(keys[index])} length key {keys[index]}" +
+#           f" (text conformity index {i_c}): \n{encoded[index]}\n")
+
+file = open("text.txt", "r", encoding="UTF-8")
 text = file.read().lower()
 
-encode_with_keys(text, alphabet)
+print(decode(list(text), list("абвгдежзийклмнопрстуфхцчшщъыьэюя"), russ_alphabet))
